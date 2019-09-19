3752e459f2f820e45b1cf774234a777cdb2d3ecb
cundo <facinditto@gmail.com>
2019-09-19 13:22:20 -0300

    falta

--- frontend/src/app/components/create-movie/create-movie.component.ts
+++ frontend/src/app/components/create-movie/create-movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { Movie } from '../../models/movie';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-movie',
@@ -19,12 +20,25 @@ export class CreateMovieComponent implements OnInit {
 
   ngOnInit() {  }
 
-  createMovie(titulo, fechaEstreno) {
-    this.movie = new Movie();
-    this.movie.titulo = titulo;
-    this.movie.fechaEstreno = fechaEstreno.toString();
-    console.log(this.movie.fechaEstreno.toString());
-    this.moviesService.postMovie(this.movie);
+  addMovie(form?: NgForm) {
+    /*if(form.value._id){
+      this.moviesService.putMovie(form.value)
+        .subscribe(res=>{
+          this.resetForm();
+        })
+    }else{*/
+      this.moviesService.postMovie(form.value)
+        .subscribe(res => {
+          this.resetForm(form);
+        })
+     // }
+
+  }
+  resetForm(form?: NgForm) {
+    if (form) {
+      form.reset();
+      this.moviesService.selectedMovies = new Movie();
+    }
   }
 
 }
--- frontend/src/app/models/movie.ts
+++ frontend/src/app/models/movie.ts
@@ -1,18 +1,24 @@
 export class Movie {
 
-constructor() {}
-    _id: string;
-    titulo: string;
-    descripcion: string;
-    poster: string;
-    fechaEstreno: string;
-    tipo: string;
-    actores: string;
-    argumento: string;
-    director: string;
-    duracion: number;
-    entradasSem: number;
-    genero: string;
-    estado: string;
-}
-
+    constructor(_id='',titulo='',fechaEstreno='') {
+            this._id=_id;
+            this.titulo=titulo;
+            this.fechaEstreno=fechaEstreno;
+    
+    }
+        _id: string;
+        titulo: string;
+        descripcion: string;
+        poster: string;
+        fechaEstreno: string;
+        tipo: string;
+        actores: string;
+        argumento: string;
+        director: string;
+        duracion: number;
+        entradasSem: number;
+        genero: string;
+        estado: string;
+    }
+    
+    
